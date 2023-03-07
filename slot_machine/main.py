2a04e345245a72f21c56852f10f69bc6138f3018
chaeuny <123149020+chaeuny@users.noreply.github.com>
2023-03-07 10:16:01 +0000

    git push

--- slot_machine/main.py
+++ slot_machine/main.py
@@ -1,7 +1,41 @@
+import random
+
 #cosntant value
 MAX_LINES = 3
 MAX_BET = 1000
 MIN_BET = 100
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2, 
+    "B": 4, 
+    "C": 6, 
+    "D": 8
+}
+
+#we need to randomize the symbols in each row and add it to the list
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    #the nested list represents the values in the column
+    columns = []
+    for _ in range(cols): #generate a col for every col we have
+        column = []
+        current_symbols = all_symbols[:] #copy of the current symbols
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
 
 #gets the input from the user
 def deposit():
