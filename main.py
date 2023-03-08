5a4d61715fd3a85798a1361a9cef563a277da841
chaeuny <123149020+chaeuny@users.noreply.github.com>
2023-03-08 14:52:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 #cosntant value
 MAX_LINES = 3
 MAX_BET = 1000
-MIN_BET = 100
+MIN_BET = 10
 
 ROWS = 3
 COLS = 3
@@ -14,6 +14,30 @@ symbol_count = {
     "C": 6, 
     "D": 8
 }
+
+#the more rare the symbol, the higher the value
+symbol_value = {
+    "A": 5, 
+    "B": 4, 
+    "C": 3, 
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winnings_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+            else: 
+                winnings += values[symbol] * bet
+                winnings_lines.append(line + 1)
+        
+    return winnings, winnings_lines
+
 
 #we need to randomize the symbols in each row and add it to the list
 def get_slot_machine_spin(rows, cols, symbols):
@@ -30,12 +54,22 @@ def get_slot_machine_spin(rows, cols, symbols):
         for _ in range(rows):
             value = random.choice(current_symbols)
             current_symbols.remove(value)
-            column.append(value)
+            column.append(value) #add the value to the column
 
         columns.append(column)
 
     return columns
 
+def print_slot_machine(columns):
+    #we need to change the column into a row: 'transposing'
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        
+        print() #brings a new line down 
 
 #gets the input from the user
 def deposit():
@@ -80,10 +114,7 @@ def get_bet():
 
     return amount
 
-
-#rerunning the main function (if they want to play again)
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True: 
         bet = get_bet()
@@ -97,6 +128,26 @@ def main():
 
     print(f"You are betting R{bet} on {lines} lines. Total bet is equal to: R{total_bet}")
 
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won R{winnings}.")
+    print(f"You won on lines:", *winning_lines) #splat opperator: pass every single line from the winning_lines list 
+    return winnings - total_bet
+
+
+#rerunning the main function (if they want to play again)
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is R{balance}")
+        answer = input("Press enter to play. (q to quit)")
+        if answer == "q":
+            break
+        else:
+            balance == spin(balance)
+
+    print(f"You are left with R{balance}")
 
 
 main()
